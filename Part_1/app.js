f07f5e31aa29ef280e0f9ab20b0ea89c4df8b10e
pkallas <pkallas@patricks-mbp.lan>
2017-08-21 13:03:46 -0700

    Added api/days/:day route to app.js in part 1

--- Part_1/app.js
+++ Part_1/app.js
@@ -1,3 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const daysOfWeek = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7
+};
+
+app.listen(3000, () => {
+  console.log('Listening on localhost://3000');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/api/days', (request, response) => {
+  response.send('Now try adding a spefic day after days using this format: /monday')
+});
+
+app.get('/api/days/:day', (request, response) => {
+  let dayOfWeek = request.params.day.toLowerCase();
+  if (daysOfWeek[dayOfWeek] === undefined) {
+    response.status(400).send(`${dayOfWeek} is not a valid day!`)
+  }
+  else {
+    response.send(`${daysOfWeek[dayOfWeek]}`)
+  }
+});
