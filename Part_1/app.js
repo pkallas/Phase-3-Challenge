1e5e5018d5c9a6eedfef58b5a0f4422c848f09f0
pkallas <pkallas@patricks-mbp.lan>
2017-08-21 13:35:51 -0700

    Added post route to app.js; moved package.json into part 1 directory as per specs

--- Part_1/app.js
+++ Part_1/app.js
@@ -15,8 +15,6 @@ app.listen(3000, () => {
   console.log('Listening on localhost://3000');
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.get('/api/days', (request, response) => {
   response.send('Now try adding a spefic day after days using this format: /monday')
 });
@@ -28,5 +26,22 @@ app.get('/api/days/:day', (request, response) => {
   }
   else {
     response.send(`${daysOfWeek[dayOfWeek]}`)
+  }
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/array/concat', (request, response) => {
+  let firstArray = request.body['array1'];
+  let secondArray = request.body['array2'];
+  if (Array.isArray(firstArray) === false) {
+    response.status(400).json({"error": "Input data should be of type Array."})
+  }
+  else if (Array.isArray(secondArray) === false) {
+    response.status(400).json({"error": "Input data should be of type Array."})
+  }
+  else {
+    let concatedArray = firstArray.concat(secondArray)
+    response.json({"result": concatedArray})
   }
 });
