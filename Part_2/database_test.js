281217a12dd5eff860357ea31c8b58a34d171a27
pkallas <pkallas@patricks-mbp.lan>
2017-08-23 10:12:04 -0700

    Made changes to tests in part 2

--- Part_2/database_test.js
+++ Part_2/database_test.js
@@ -1,8 +1,54 @@
 process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
-const realShoppers = require('./database').realShoppers;
-const shopperOrders = require('./database').shopperOrders;
-const productList = require('./database').productList;
+const client = require('./pg');
+let input = '';
+const productText = `SELECT name, section FROM products WHERE section = '${input}'`;
+const ordersText = `SELECT order_id, total_cost FROM orders WHERE shopper_id = '${input}'`;
+const shoppersText = `SELECT name, COUNT(*) FROM orders
+  JOIN shoppers ON shoppers.id = orders.shopper_id
+  GROUP BY name`;
+const productList = () => {
+  let productsNameSection = [];
+  client.query(productText)
+  .then(result => {
+    result.rows.map(index => {
+      productsNameSection.push(index.name);
+      productsNameSection.push(index.section);
+    })
+    return productsNameSection;
+    client.end();
+  })
+  .catch(error => console.log(error))
+};
+
+const shopperOrders = () => {
+  let ordersIdCost = [];
+  client.query(ordersText)
+  .then(result => {
+    result.rows.map(index => {
+      ordersIdCost.push(index.order_id);
+      ordersIdCost.push(index.total_cost);
+    })
+    return ordersIdCost;
+    client.end();
+  })
+  .catch(error => console.log(error))
+};
+
+const realShoppers = () => {
+  let shoppersNameCount = [];
+  client.query(shoppersText)
+  .then(result => {
+    result.rows.map(index => {
+      shoppersNameCount.push(index.name);
+      shoppersNameCount.push(index.count);
+    })
+    return shoppersNameCount;
+    client.end();
+  })
+  .catch(error => console.log(error))
+};
+
 
 describe('realShoppers', function() {
   it('Should return an array of data from the db', function(done) {
